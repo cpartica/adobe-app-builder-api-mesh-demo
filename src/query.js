449d995ef0ea70a587609a44fef4f8066d61d5fc
Cristian Partica <cpartica@users.noreply.github.com>
2023-11-05 19:12:05 +0000

    Update App.js and query.js

--- src/query.js
+++ src/query.js
@@ -1,29 +1,23 @@
 const SEARCH_TERM = "tops";
 const SORT = "DESC";
-const NUMBER_OF_RESULTS = 6;
+const NUMBER_OF_RESULTS = 18;
 
 //    products(search: "${SEARCH_TERM}") {
 
 const query = {
   query: `{
-    products(search: "${SEARCH_TERM}", sort: { name: ${SORT} },  pageSize: 18) {
+    products(search: "${SEARCH_TERM}", sort: { name: ${SORT} },  pageSize: ${NUMBER_OF_RESULTS}) {
       items {
         name
         sku
         price_range {
           minimum_price {
-            # we don't need this anymore # discount { percent_off }
+            discount { percent_off }
             regular_price { value }
-            # we don't need this anymore #  final_price { value }
+            final_price { value }
           }
         }
-        ... on ConfigurableProduct {
-          discounted_price
-          discount_percentage
-          inventory_details {
-            sku location quantity
-          }
-        }
+        stock_status
         image { url }
       }
     }
